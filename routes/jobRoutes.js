f6fdc685a94be6b63aa15a79431cf1d66169798a
Aman Dwivedi <amandwivedi71124.com>
2024-08-21 14:12:08 +0530

    added

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getJobs, createJob, updateJob, deleteJob } = require('../controllers/jobController');
-const { protect,admin } = require('../middleware/Authmiddleware');
+const { protect,admin } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
